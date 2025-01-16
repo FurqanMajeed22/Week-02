292367f605e9d058d27ea284cc0ac5b3a4b3a9ff
unknown <syedfurkan999@gmail.com.com>
2025-01-16 13:11:29 +0530

    Day-01

---
+++ Day-01/Level 1/Book.java
@@ -0,0 +1,25 @@
+class Book {
+    private String title;
+    private String author;
+    private double price;
+    // Constructor 
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Book Details:");
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    // Main method to test the Book class
+     public static void main(String[] args) {
+        
+        Book book = new Book("Capgemini", "Furqan", 2000);
+        book.displayDetails();
+    }
+}
