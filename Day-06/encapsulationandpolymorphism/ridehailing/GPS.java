33195d201cfe91175e1aa4b17aa7e0be4c051cf8
unknown <syedfurkan999@gmail.com.com>
2025-01-26 14:28:30 +0530

    Adding EncapsulationandPolymorphism

---
+++ Day-06/encapsulationandpolymorphism/ridehailing/GPS.java
@@ -0,0 +1,8 @@
+package encapsulationandpolymorphism.ridehailing;
+
+
+// Interface for GPS functionality
+interface GPS {
+    String getCurrentLocation();
+    void updateLocation(String newLocation);
+}
