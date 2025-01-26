ed36a1c0be8ac08140b5c219d41a51c1e2be786e
unknown <syedfurkan999@gmail.com.com>
2025-01-26 14:27:50 +0530

    Adding EncapsulationandPolymorphism

---
+++ Day-06/encapsulationandpolymorphism/hospitalmanagement/MedicalRecord.java
@@ -0,0 +1,6 @@
+package encapsulationandpolymorphism.hospitalmanagement;
+// Interface MedicalRecord
+interface MedicalRecord {
+    void addRecord(String record);
+    String viewRecords();
+}
---
+++ Day-06/encapsulationandpolymorphism/hospitalmanagement/Patient.java
@@ -0,0 +1,19 @@
+package encapsulationandpolymorphism.hospitalmanagement;
+// Abstract class Patient
+abstract class Patient {
+    String patientId;
+    String name;
+    int age;
+    // Constructor
+    public Patient(String patientId, String name, int age) {
+        this.patientId = patientId;
+        this.name = name;
+        this.age = age;
+    }
+    // Abstract method to calculate the bill
+    abstract double calculateBill();
+    // Concrete method to get patient details
+    public String getPatientDetails() {
+        return "Patient ID: " + patientId + ", Name: " + name + ", Age: " + age;
+    }
+}
